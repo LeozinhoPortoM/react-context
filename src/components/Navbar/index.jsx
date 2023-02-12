34762db5839a7b2336473f4374e45a1b3d573b18
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-12 16:42:40 -0300

    setting style navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,13 +1,13 @@
 import "./style.css"
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About</Link>
-      <Link to={"/contact"}>Contact</Link>
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"/about"}>About</NavLink>
+      <NavLink to={"/contact"}>Contact</NavLink>
     </nav>
   );
 }
