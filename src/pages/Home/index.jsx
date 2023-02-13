770c16c18e0d04446294e0fb08fabb248f078c5f
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-13 10:47:44 -0300

    using new hook with context

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,28 @@
 import ChangeCounter from "../../components/ChangeCounter";
 
 import { useCounterContext } from "../../hooks/useCounterContext";
+import { useTitleColorContext } from "../../hooks/useTitleColorContext";
 
 export default function Home() {
   const { counter } = useCounterContext();
+
+  const { color, dispatch } = useTitleColorContext();
+
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
+
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Valor do contador: {counter}</p>
 
       <ChangeCounter />
+
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
     </div>
   );
 }
