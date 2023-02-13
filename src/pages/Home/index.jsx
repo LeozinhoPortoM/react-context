58d7967c9693c00fb6e464c94692c7a79e5c814b
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-13 09:39:08 -0300

    using context and adding ChangeCounter component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import ChangeCounter from "../../components/ChangeCounter";
+import { CounterContext } from "../../context/CounterContext";
+
 export default function Home() {
+  const { counter } = useContext(CounterContext);
   return (
     <div className="home">
       <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
+
+      <ChangeCounter />
     </div>
   );
 }
