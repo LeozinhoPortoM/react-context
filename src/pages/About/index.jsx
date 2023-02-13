5b201670840095747e8e4f32aaeaea9e3f68dbfe
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-02-13 09:40:53 -0300

    adding context to pages

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { CounterContext } from "../../context/CounterContext";
+
 export default function About() {
+  const { counter } = useContext(CounterContext);
   return (
     <div className="about">
       <h1>About</h1>
+      <p>Valor do contador: {counter}</p>
     </div>
   );
 }
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { CounterContext } from "../../context/CounterContext";
+
 export default function Contact() {
+  const { counter } = useContext(CounterContext);
   return (
     <div className="contact">
       <h1>Contact</h1>
+      <p>Valor do contador: {counter}</p>
     </div>
   );
 }
